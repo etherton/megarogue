353c07971786f6bbb8fc358f3e06b61f581a7dec
David Etherton <david.c.etherton@gmail.com>
2023-08-20 20:52:05 +0100

    Added location of toolchain

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-# ../llvm-project/.build/bin/clang -target m68k-apple-macosx13.0.0 -c vectors.c header.c start.c joypad.c video.c
-#../llvm-project/.build/bin/clang -DNDEBUG -target m68k-apple-macosx13.0.0 -fomit-frame-pointer -O3 -c vectors.c header.c start.c joypad.c video.c
-# ../llvm-project/.build/bin/ld.lld -T md.script --oformat=binary vectors.o header.o start.o video.o joypad.o
+# brew install rosco-m68k/toolchain/binutils-cross-m68k
+# brew install rosco-m68k/toolchain/gcc-cross-m68k@13  
 
 /opt/homebrew/bin/m68k-elf-gcc -DNDEBUG -Wno-multichar -march=68000 -fomit-frame-pointer -Os -c vectors.c header.c start.c joypad.c video.c && \
 /opt/homebrew/bin/m68k-elf-ld -T md.script --oformat=binary vectors.o header.o start.o video.o joypad.o -o megarogue.rom && \
