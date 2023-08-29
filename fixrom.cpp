7dab341577c04c45184bdebc185c284d4c86a7c8
David Etherton <david.c.etherton@gmail.com>
2023-08-29 21:23:04 +0100

    sigh, realized the code wasn't dealing with endian issues at all either

--- fixrom.cpp
+++ fixrom.cpp
@@ -23,14 +23,16 @@ int main(int argc,char **argv) {
 	memset(rom,0,rounded);
 	fread(rom,1,size,f);
 	fseek(f,0,SEEK_SET);
-	uint16_t *rom16 = (uint16_t*) rom;
-	uint32_t *rom32 = (uint32_t*) rom;
 	uint16_t checksum = 0;
-	for (size_t i=256; i<rounded/2; i++)
-		checksum += rom16[i];
-	rom16[0x18E/2] = checksum;
-	rom32[0x1a4/4] = (uint32_t)rounded-1;
-	printf("checksum set to %u\n",rom16[0x18e/2]);
+	for (size_t i=512; i<rounded; i+=2)
+		checksum += (rom[i]<<8) + rom[i+1];
+	rom[0x18E] = checksum >> 8;
+	rom[0x18F] = checksum;
+	rom[0x1a4] = ((rounded-1)>>24);
+	rom[0x1a5] = ((rounded-1)>>16);
+	rom[0x1a6] = ((rounded-1)>>8);
+	rom[0x1a7] = (rounded-1);
+	printf("checksum set to 0x%04x\n",checksum);
 	fwrite(rom,1,rounded,f);
 	fclose(f);
 	return 0;
