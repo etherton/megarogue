b07470a8c94fefc10c849947dc47e24648f2f3de
David Etherton <david.c.etherton@gmail.com>
2023-09-30 07:47:36 +0100

    added missing files

---
+++ huffman_decode.cpp
@@ -0,0 +1,4 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <assert.h>
+#include "huffman_decode.c"
---
+++ stdint.h
@@ -0,0 +1,8 @@
+
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long size_t;
+
+#define assert(...)
